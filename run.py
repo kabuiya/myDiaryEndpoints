7f7994200998a60610d2f6a8434ca84daa138820
Monicah <kabuiyamonicah@gmail.com>
2025-06-26 23:01:08 +0300

    updated cors in run.py

--- run.py
+++ run.py
@@ -38,7 +38,7 @@ def create_app():
 
     # CORS(app, origins=["http://localhost:5000"])
 
-    CORS(app, origins=["https://diary-163ea.web.app"], supports_credentials=True)
+    CORS(app, origins=["https://diary-163ea.web.app, http://127.0.0.1:5500"], supports_credentials=True)
 
     return app
 
